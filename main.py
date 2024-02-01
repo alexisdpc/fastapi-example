c625080e63e4aca7097fcc105f7816251527982a
alexisdpc <aplscenciac@gmail.com>
2024-02-01 13:39:08 +0100

    up

--- main.py
+++ main.py
@@ -14,5 +14,12 @@ async def add(num1: int, num2: int):
     total = num1 + num2
     return {"total": total}
 
+@app.get("/substract/{num1}/{num2}")
+async def add(num1: int, num2: int):
+    """Add two numbers together"""
+
+    total = num1 - num2
+    return {"total": total}
+
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
